73df52c0dfb8cf59e6bc02ed03b92dcafa8dda2f
TheCandyQueen <73914684+TheCandyQueen@users.noreply.github.com>
2021-02-07 18:36:34 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function multiply() {
+    var answer = Number(document.getElementById("firstno").value) *
+        Number(document.getElementById("firstno").value)
+    window.alert("Area = " + answer)
+  }
+
+  function perimiter() {
+      var perimiterAnswer = Number(document.getElementById("secondno").value) * (4)
+      window.alert("Perimiter = " + perimiterAnswer)
+  }
